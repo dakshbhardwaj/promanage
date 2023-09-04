69df3912014afae5c9f83e4f9c39091ce973e61c
dakshbhardwaj <dakshbhardwaj2@gmail.com>
2023-09-04 12:55:05 +0530

    added bootstrap and add project formm

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
-import TestComponent from "../components/testComponent";
+import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
