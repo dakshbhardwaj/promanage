5458e05b0ecff1b81fc22367f1c50c6437dad8d9
mohitcharkha <mohit@plgworks.com>
2023-09-05 11:08:09 +0530

    Added People Suggestion List on project submit

--- components/AddProjectForm.js
+++ components/AddProjectForm.js
@@ -7,8 +7,31 @@ function AddProjectForm() {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [estimatedDeliveryDate, setEstimatedDeliveryDate] = useState("");
+  const [showSuggestions, setShowSuggestion] = useState(false);
 
   const [errors, setErrors] = useState({});
+  const [employees, setEmployees] = useState([
+    {
+      id: "1",
+      name: "John Doe",
+      email: "john.doe@example.com",
+      designation: "Software Engineer",
+    },
+    {
+      id: "2",
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+      designation: "UI Designer",
+    },
+    {
+      id: "3",
+      name: "Bob Johnson",
+      email: "bob.johnson@example.com",
+      designation: "Project Manager",
+    },
+  ]);
+
+  let [acceptedEmployees, setAcceptedEmployee] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -50,108 +73,174 @@ function AddProjectForm() {
         startDate,
         endDate,
       };
+      setShowSuggestion(true);
       console.log("Form data:", formData);
     } else {
       setErrors(newErrors);
     }
   };
 
+  const removeEmployee = (index) => {
+    employees.splice(index, 1);
+    setEmployees(employees);
+  };
+
+  const acceptEmployee = (employeeId) => {
+    acceptedEmployees.push(employeeId);
+    setAcceptedEmployee(acceptedEmployees);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="name"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        {errors.name && <p className="text-danger">{errors.name}</p>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="description" className="form-label">
-          Description
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="description"
-          placeholder="Enter description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        {errors.description && (
-          <p className="text-danger">{errors.description}</p>
-        )}
-      </div>
-      <div className="form-group">
-        <label htmlFor="company" className="form-label">
-          Company
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="company"
-          placeholder="Enter company"
-          value={company}
-          onChange={(e) => setCompany(e.target.value)}
-        />
-        {errors.company && <p className="text-danger">{errors.company}</p>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="startDate" className="form-label">
-          Start Date
-        </label>
-        <input
-          type="date"
-          className="form-control"
-          id="startDate"
-          placeholder="Enter start date"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-        />
-        {errors.startDate && <p className="text-danger">{errors.startDate}</p>}
-      </div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={showSuggestions}
+          />
+          {errors.name && <p className="text-danger">{errors.name}</p>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            placeholder="Enter description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            disabled={showSuggestions}
+          />
+          {errors.description && (
+            <p className="text-danger">{errors.description}</p>
+          )}
+        </div>
+        <div className="form-group">
+          <label htmlFor="company" className="form-label">
+            Company
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="company"
+            placeholder="Enter company"
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+            disabled={showSuggestions}
+          />
+          {errors.company && <p className="text-danger">{errors.company}</p>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="startDate" className="form-label">
+            Start Date
+          </label>
+          <input
+            type="date"
+            className="form-control"
+            id="startDate"
+            placeholder="Enter start date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            disabled={showSuggestions}
+          />
+          {errors.startDate && (
+            <p className="text-danger">{errors.startDate}</p>
+          )}
+        </div>
 
-      <div className="form-group">
-        <label htmlFor="estimatedDeliveryDate" className="form-label">
-          Estimated Delivery Date
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="estimatedDeliveryDate"
-          placeholder="Enter estimated delivery date"
-          value={estimatedDeliveryDate}
-          onChange={(e) => setEstimatedDeliveryDate(e.target.value)}
-        />
-        {errors.estimatedDeliveryDate && (
-          <p className="text-danger">{errors.estimatedDeliveryDate}</p>
-        )}
-      </div>
+        <div className="form-group">
+          <label htmlFor="estimatedDeliveryDate" className="form-label">
+            Estimated Delivery Date
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="estimatedDeliveryDate"
+            placeholder="Enter estimated delivery date"
+            disabled={showSuggestions}
+            value={estimatedDeliveryDate}
+            onChange={(e) => setEstimatedDeliveryDate(e.target.value)}
+          />
+          {errors.estimatedDeliveryDate && (
+            <p className="text-danger">{errors.estimatedDeliveryDate}</p>
+          )}
+        </div>
 
-      <div className="form-group">
-        <label htmlFor="endDate" className="form-label">
-          End Date
-        </label>
-        <input
-          type="date"
-          className="form-control"
-          id="endDate"
-          placeholder="Enter end date"
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
-        />
-        {errors.endDate && <p className="text-danger">{errors.endDate}</p>}
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+        <div className="form-group">
+          <label htmlFor="endDate" className="form-label">
+            End Date
+          </label>
+          <input
+            type="date"
+            className="form-control"
+            id="endDate"
+            placeholder="Enter end date"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+            disabled={showSuggestions}
+          />
+          {errors.endDate && <p className="text-danger">{errors.endDate}</p>}
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      {showSuggestions ? (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Designation</th>
+                <th>Add</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.map((employee, index) => (
+                <tr key={index}>
+                  <td>{employee.name}</td>
+                  <td>{employee.email}</td>
+                  <td>{employee.designation}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        acceptEmployee(employee.id);
+                      }}
+                      disabled={acceptedEmployees.includes(employee.id)}
+                    >
+                      {acceptedEmployees.includes(employee.id)
+                        ? "Added"
+                        : "Add"}
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        removeEmployee(index);
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : null}
+    </div>
   );
 }
 
